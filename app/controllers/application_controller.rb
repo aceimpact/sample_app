58df5ddcaf8c3208b3f3d98d3fe94dda8e10dc96
aceimpact <kei2613@icloud.com>
2018-10-03 19:31:13 +0900

    device install

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+
 
   def hello
     render html: "Hello,world!"
   end
+
+protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:email, :password, :password_confirmation, :name)}
+
+    devise_parameter_sanitizer.permit(:sign_in){|u| u.permit(:user_id, :password, :remember_me)}
+  end
+
 end
